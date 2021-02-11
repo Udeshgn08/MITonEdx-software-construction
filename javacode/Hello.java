6b39836a434f8bb1f21caaa8fa787cbeee49aa48
Udeshgn08 <udeshkumargn08j@gmail.com>
2021-02-11 17:29:39 +0100

    Make sure dev-setup works fine

---
+++ javacode/Hello.java
@@ -0,0 +1,11 @@
+package javacode;
+
+public class Hello {
+
+    public static void main(String[] args) { 
+        // System Java built-in class
+        System.out.println("Hello..hello");
+
+    }
+
+}
